9be7a5a319e0c28c8495b82185e7f5c84d30d618
Kasitskaya Elena <keal.gyurie7@yandex.ru>
2020-08-26 00:23:24 +0300

    Updates

---
+++ precache-manifest.08671862e5db69c665192fe5471074b4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78a0ecac9bdf6ddfbcb2d1d5964c68a",
+    "url": "/mesto-react/index.html"
+  },
+  {
+    "revision": "1b27516e905d866c368d",
+    "url": "/mesto-react/static/css/main.e1fe6575.chunk.css"
+  },
+  {
+    "revision": "64b9e99119d99a59aa54",
+    "url": "/mesto-react/static/js/2.458dbe58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mesto-react/static/js/2.458dbe58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b27516e905d866c368d",
+    "url": "/mesto-react/static/js/main.7d2fb2ca.chunk.js"
+  },
+  {
+    "revision": "6e04319a44e5e6859875",
+    "url": "/mesto-react/static/js/runtime-main.36fb0e45.js"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/mesto-react/static/media/Closecon.5df32a06.svg"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/mesto-react/static/media/addbutton.b2fce4ac.svg"
+  },
+  {
+    "revision": "5d63eaa56e8f31cd6478ee7751813fed",
+    "url": "/mesto-react/static/media/deletebutton.5d63eaa5.svg"
+  },
+  {
+    "revision": "30fc72583a37b57d4cb4d31a1a6b649e",
+    "url": "/mesto-react/static/media/deletebuttonup.30fc7258.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/mesto-react/static/media/editbutton.6bfdb354.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "/mesto-react/static/media/group.83a46f45.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/mesto-react/static/media/like.5bbb05f7.svg"
+  }
+]);
